09174d702c14bf5d02f84ab728803107243891b2
ajiemoon <moonaziiz4@gmail.com>
2026-01-20 20:46:47 +0700

    update ajie

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,86 +1,46 @@
 import React from "react";
-import LogoVPN from "../../public/assets/Logo.svg";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
+
 const Footer = () => {
   return (
     <div className="bg-white-300 pt-44 pb-24">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <LogoVPN className="h-8 w-auto mb-6" />
+          <span className="font-bold text-2xl text-indigo-600 mb-6 tracking-tight">TECHCONNECT</span>
           <p className="mb-4">
-            <strong className="font-medium">LaslesVPN</strong> is a private
-            virtual network that has unique features and has high security.
+            <strong className="font-medium text-indigo-600">TechConnect</strong> adalah penyedia solusi IT terpercaya untuk pembuatan platform digital dan infrastruktur jaringan internet.
           </p>
           <div className="flex w-full mt-2 mb-8 -mx-2">
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md cursor-pointer hover:bg-indigo-600 transition-all">
               <Facebook className="h-6 w-6" />
             </div>
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md cursor-pointer hover:bg-indigo-600 transition-all">
               <Twitter className="h-6 w-6" />
             </div>
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md cursor-pointer hover:bg-indigo-600 transition-all">
               <Instagram className="h-6 w-6" />
             </div>
           </div>
-          <p className="text-gray-400">©{new Date().getFullYear()} - LaslesVPN</p>
+          <p className="text-gray-400">©{new Date().getFullYear()} - TechConnect Indonesia</p>
         </div>
         <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
+          <p className="text-black-600 mb-4 font-medium text-lg">Layanan</p>
           <ul className="text-black-500 ">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Download{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Pricing{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Locations{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Server{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Countries{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Blog{" "}
-            </li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">Web Bisnis</li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">Landing Page</li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">WiFi Kantor</li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">WiFi Hotspot</li>
           </ul>
         </div>
         <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
+          <p className="text-black-600 mb-4 font-medium text-lg">Perusahaan</p>
           <ul className="text-black-500">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              LaslesVPN ?{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              FAQ{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Tutorials{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              About Us{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Privacy Policy{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Terms of Service{" "}
-            </li>
-          </ul>
-        </div>
-        <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
-          <ul className="text-black-500">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Affiliate{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Become Partner{" "}
-            </li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">Tentang Kami</li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">FAQ</li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">Karir</li>
+            <li className="my-2 hover:text-indigo-600 cursor-pointer transition-all">Kontak</li>
           </ul>
         </div>
       </div>
--- components/Testimoni.js
+++ components/Testimoni.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-
-// import react slick
 import Slider from "react-slick";
 import Image from "next/image";
 import Stars from "../public/assets/Icon/stars.svg";
@@ -10,129 +8,76 @@ import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
 const Testimoni = ({
   listTestimoni = [
     {
-      name: "iezh Robert",
+      name: "Andi Saputra",
       image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+      city: "Jakarta",
+      country: "Indonesia",
+      rating: "5.0",
+      testimoni: "Website kafe saya jadi jauh lebih profesional dan pemasangan WiFi-nya sangat rapi. Pelanggan sekarang puas dengan koneksinya!",
     },
     {
-      name: "iezh Robert",
+      name: "Siska Amelia",
       image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+      city: "Bandung",
+      country: "Indonesia",
+      rating: "4.8",
+      testimoni: "Sangat responsif! Tim TechConnect membantu setting jaringan kantor kami yang awalnya lambat jadi kencang kembali.",
     },
     {
-      name: "iezh Robert",
+      name: "Budi Santoso",
       image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    },
-    {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    },
+      city: "Surabaya",
+      country: "Indonesia",
+      rating: "4.9",
+      testimoni: "Jasa pembuatan landing page terbaik dengan harga terjangkau. SEO-nya benar-benar bekerja, toko saya makin ramai.",
+    }
   ],
 }) => {
   const settings = {
     dots: true,
-    customPaging: function (i) {
-      return (
-        <a className="">
-          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
-        </a>
-      );
-    },
-    dotsClass: "slick-dots w-max absolute mt-20  ",
+    customPaging: (i) => <a className=""><span className="mx-2 rounded-full h-4 w-4 block cursor-pointer transition-all bg-indigo-200 active:bg-indigo-600"></span></a>,
+    dotsClass: "slick-dots w-max absolute mt-20",
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 2,
+    slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 770,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
+      { breakpoint: 770, settings: { slidesToShow: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 1 } },
     ],
   };
   const [sliderRef, setSliderRef] = useState(null);
 
   return (
     <>
-      <Slider
-        {...settings}
-        arrows={false}
-        ref={setSliderRef}
-        className="flex items-stretch justify-items-stretch"
-      >
-        {listTestimoni.map((listTestimonis, index) => (
+      <Slider {...settings} arrows={false} ref={setSliderRef} className="flex items-stretch justify-items-stretch">
+        {listTestimoni.map((item, index) => (
           <div className="px-3 flex items-stretch" key={index}>
-            <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
+            <div className="border-2 border-gray-500 hover:border-indigo-500 transition-all rounded-lg p-8 flex flex-col">
               <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
                 <div className="flex order-2 xl:order-1">
-                  <Image
-                    src={listTestimonis.image}
-                    height={50}
-                    width={50}
-                    alt="Icon People"
-                  />
+                  <Image src={item.image} height={50} width={50} alt="User" />
                   <div className="flex flex-col ml-5 text-left">
-                    <p className="text-lg text-black-600 capitalize">
-                      {listTestimonis.name}
-                    </p>
-                    <p className="text-sm text-black-500 capitalize">
-                      {listTestimonis.city},{listTestimonis.country}
-                    </p>
+                    <p className="text-lg text-black-600 capitalize font-medium">{item.name}</p>
+                    <p className="text-sm text-black-500 capitalize">{item.city}, {item.country}</p>
                   </div>
                 </div>
                 <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
-                  <p className="text-sm">{listTestimonis.rating}</p>
-                  <span className="flex ml-4">
-                    <Stars className="h-4 w-4" />
-                  </span>
+                  <p className="text-sm">{item.rating}</p>
+                  <span className="flex ml-4"><Stars className="h-4 w-4" /></span>
                 </div>
               </div>
-              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
+              <p className="mt-5 text-left italic">“{item.testimoni}”</p>
             </div>
           </div>
         ))}
       </Slider>
       <div className="flex w-full items-center justify-end">
         <div className="flex flex-none justify-between w-auto mt-14">
-          <div
-            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
-            onClick={sliderRef?.slickPrev}
-          >
+          <div className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-indigo-600 border hover:bg-indigo-600 hover:text-white-500 transition-all text-indigo-600 cursor-pointer" onClick={sliderRef?.slickPrev}>
             <ArrowBack className="h-6 w-6 " />
           </div>
-          <div
-            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
-            onClick={sliderRef?.slickNext}
-          >
+          <div className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-indigo-600 border hover:bg-indigo-600 hover:text-white-500 transition-all text-indigo-600 cursor-pointer" onClick={sliderRef?.slickNext}>
             <ArrowNext className="h-6 w-6" />
           </div>
         </div>
